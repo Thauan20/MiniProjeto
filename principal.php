0fa52419b78899a909ecf5088af9e791f52772db
Thauan20 <63799625+Thauan20@users.noreply.github.com>
2020-05-14 14:42:34 -0300

    Conteúdo principal
    Conteúdo principal do sit.

---
+++ principal.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <h2 class="page-header">Ventture Store</h2>
+
+    <div class="col-md-12">
+      <div class="panel">
+        <div class="panel-heading">
+          <h3>Crie você mesmo seu proprio pradrão.</h3> 
+        </div>
+        <div class="col-md-6 col-serv">
+          <h4>Serviços</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-6 col-proj">
+          <h4>Projetos</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>    
+    </div>
+  </div>
