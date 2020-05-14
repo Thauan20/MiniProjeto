dccf174fbaf9d7e9610158a17c4912288a0da676
Thauan20 <63799625+Thauan20@users.noreply.github.com>
2020-05-14 14:41:42 -0300

    Menu do projeto
    Menu simples do meu site.

---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar-center">
+  <div class="container">
+    <div class="navbar-header">
+      <ul class="nav nav-justified">
+        <li><a href="?pg=principal">Home</a></li>
+        <li><a href="?pg=produtos">Produtos</a></li>
+        <li><a href="?pg=faleconosco">Fale conosco</a></li>
+        <li><a href="?pg=servicos">Serviços</a></li>
+        
+
+      
+        <!--
+    		<li><a href="?pg=quemsomos">Quem Somos</a></li>
+    		<li><a href="?pg=produtos">Produtos</a></li>
+    		<li><a href="?pg=servicos">Serviços</a></li>
+    		<li><a href="?pg=faleconosco">Fale conosco</a></li>
+        -->
+      </ul>
+    </div>
+  </div>
+</nav>
