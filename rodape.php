19107afc17fe7fb3a8308be36285135acf28e7d5
Thauan20 <63799625+Thauan20@users.noreply.github.com>
2020-05-21 10:18:08 -0300

    rodapé do site

--- rodape.php
+++ rodape.php
@@ -1,4 +1,15 @@
-<footer>
-    <hr>
-    <center>2020 &copy; MeuSite.com - Direitos Reservados </center>
-</footer>
+<!-- Rodapé -->
+<div class="footer">
+  Thauan Rodrigues - Direitos Abertos.
+</div>
+<style>
+/* rodapé */
+.footer {
+   min-width: : 280px;
+  padding: 10px;
+  text-align: center;
+  background: #333;
+  color: white;
+  font-size: 13px;
+}
+</style>
